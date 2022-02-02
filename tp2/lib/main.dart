8d213f756576ceaf2833007b13181e1264fc6ff2
johanneclc <johannecalcoen@gmail.com>
2022-02-02 12:11:10 +0100

    Exercice1 done

--- tp2/lib/main.dart
+++ tp2/lib/main.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: const MyApp(), 
+  ));
 }
 
+//HOME
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TP2',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'TP2'),
     );
   }
 }
@@ -29,38 +32,189 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: NavDrawer(),
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Text("Welcome")
+    );
+  }
+}
+
+//EXERCICE 1
+class MyApp1 extends StatelessWidget {
+  const MyApp1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TP2',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      home: const Exercice1(title: 'TP2'),
+    );
+  }
+}
+
+class Exercice1 extends StatefulWidget {
+  const Exercice1({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<Exercice1> createState() => _Exercice1State();
+}
+
+class _Exercice1State extends State<Exercice1> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: NavDrawer(),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Image.network('https://picsum.photos/512/1024')
+    );
+  }
+}
+
+//EXERCICE 2
+class MyApp2 extends StatelessWidget {
+  const MyApp2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TP2',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Exercice2(title: 'TP2'),
+    );
+  }
+}
+
+
+class SliderWidget extends StatefulWidget {
+  const SliderWidget({Key? key}) : super(key: key);
+
+  @override
+  State<SliderWidget> createState() => _SliderWidgetState();
+}
+
+class _SliderWidgetState extends State<SliderWidget> {
+  double _currentSliderValue = 20;
+
+  @override
+  Widget build(BuildContext context) {
+    return Slider(
+      value: _currentSliderValue,
+      max: 100,
+      divisions: 5,
+      label: _currentSliderValue.round().toString(),
+      onChanged: (double value) {
+        setState(() {
+          _currentSliderValue = value;
+        });
+      },
+    );
+  }
+}
+
+class Exercice2 extends StatefulWidget {
+  const Exercice2({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<Exercice2> createState() => _Exercice2State();
+}
+
+class _Exercice2State extends State<Exercice2> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: NavDrawer(),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body:
+            ListView(
+                padding: const EdgeInsets.all(8),
+                children: <Widget>[
+                  Container(
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(color: Colors.white),
+                      child: Image.network('https://picsum.photos/512/1024'),
+                  ),
+                  Row(
+                    children : [
+                      Expanded(
+                        child: Text("orientation x :"), 
+                      ),
+                      Expanded(
+                        child: SliderWidget(),
+                      )                      
+                    ]
+                  )
+                ],
+              )
+                               
+    );
+  }
+}
+
+
+
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              'Menu TP2',
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+            decoration: BoxDecoration(
+                color: Colors.green,
+            )
+          ),
+          ListTile(
+            title: Text('Exercice 1 : Afficher une image'),
+            onTap: () => {Navigator.push(context, new MaterialPageRoute(builder: (context) => new MyApp1()))},
+          ),
+          ListTile(
+            title: Text('Exercice 2 : Transformer une image'),
+            onTap: () => {Navigator.push(context, new MaterialPageRoute(builder: (context) => new MyApp2()))},
+          ),
+          ListTile(
+            title: Text('Exercice 3'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            title: Text('Exercice 4'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            title: Text('Exercice'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            title: Text('Retour'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+        ],
+      ),
     );
   }
 }
